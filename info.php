d5330a2308560a5582d6d1000f95ccc6c992948e
Luiz <luizgustav796@gmail.com>
2025-06-05 08:38:17 -0300

    info page
    info page

---
+++ info.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receita_federal</title>
+    <link rel="stylesheet" href="/css/page.css">
+</head>
+<body>
+    <!-- Logo da receita federal-->
+    <div class="icon">
+        <img src="/images/Receita_federal.jpeg" alt="Ícone Receita Federal">
+    </div>
+    <!-- Links -->
+    <div class="rotulo">
+        <a href="/hompage.html">Receita Federal</a>
+        <a href="/cadastro.html">Declarar imposto?</a>
+        <a title="em desenvolvimento" href="/hompage.html">Precisa de ajuda?</a>
+     
+    </div>
+    <div class="aviso">
+        <h1>Atenção ao prazo de declaração de inposto de renda</h1>
+    </div>
+    <div class="aviso2">
+        <img src="/images/prazo_imposto.jpeg" alt="">
+    </div>
+    <div class="texto">
+         <section>
+        <h1>Quem deve se declarar?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit <br>. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur dolor facilis, fugiat autem laboriosam<br> alias ipsa mollitia provident, quod voluptates rerum recusandae!Lorem ipsum dolor sit amet consectetur adipisicing<br> elit. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur dolor facilis, fugiat<br> autem laboriosam alias ipsa mollitia provident, quod voluptates rerum recusandae!Lorem ipsum dolor sit amet<br> consectetur adipisicing elit. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur </p>
+        </section>
+ 
+        <section>
+        <h1>Quais os documentos <br>
+        necessarios?</h1>
+         <p title="documentos">Lorem ipsum dolor sit amet consectetur adipisicing elit <br>. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur dolor facilis, fugiat autem laboriosam<br> alias ipsa mollitia provident, quod voluptates rerum recusandae!Lorem ipsum dolor sit amet consectetur adipisicing<br> elit. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur dolor facilis, fugiat<br> autem laboriosam alias ipsa mollitia provident, quod voluptates rerum recusandae!Lorem ipsum dolor sit amet<br> consectetur adipisicing elit. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur </p>
+        </section>
+ 
+        <section>
+        <h1>Declaração conjunta ou <br>
+        separada?</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit <br>. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur dolor facilis, fugiat autem laboriosam<br> alias ipsa mollitia provident, quod voluptates rerum recusandae!Lorem ipsum dolor sit amet consectetur adipisicing<br> elit. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur dolor facilis, fugiat<br> autem laboriosam alias ipsa mollitia provident, quod voluptates rerum recusandae!Lorem ipsum dolor sit amet<br> consectetur adipisicing elit. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur </p>
+        </section>
+    </div>
+<div class="texto2">
+    <section>
+        <h1>Quem pode ser declarado
+        <br> como dependente?</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit <br>. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur dolor facilis, fugiat autem laboriosam<br> alias ipsa mollitia provident, quod voluptates rerum recusandae!Lorem ipsum dolor sit amet consectetur adipisicing<br> elit. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur dolor facilis, fugiat<br> autem laboriosam alias ipsa mollitia provident, quod voluptates rerum recusandae!Lorem ipsum dolor sit amet<br> consectetur adipisicing elit. Accusantium, tempore consectetur nobis tempora tenetur animi quia consequuntur </p>
+</section>
+</div>
+</body>
+</html>
+ 
